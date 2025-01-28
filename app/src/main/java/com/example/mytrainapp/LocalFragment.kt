0dab018bd6f95520ea96339b601761521905d96a
Mayur Totre <mayurtotre039@gmail.com>
2025-01-28 12:48:36 +0530

    code update

--- app/src/main/java/com/example/mytrainapp/LocalFragment.kt
+++ app/src/main/java/com/example/mytrainapp/LocalFragment.kt
@@ -5,55 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.mytrainapp.databinding.FragmentLocalBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LocalFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LocalFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentLocalBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentLocalBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_local, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LocalFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LocalFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.swapImage.setOnClickListener {
+            val startLoc = binding.etFromStation.text
+            val toLocation = binding.etToStation.text
+
+            binding.etToStation.setText(startLoc)
+            binding.etFromStation.setText(toLocation)
+        }
+
+        binding.btnClose.setOnClickListener {
+            binding.etFromStation.text.clear()
+        }
+        binding.btnCloseToStation.setOnClickListener {
+            binding.etToStation.text.clear()
+        }
+
+        binding.displayMap.setOnClickListener {
+            val dialog = BottomSheetDialog(requireContext())
+
+            val view = layoutInflater.inflate(R.layout.map_display_bottomsheet, null)
+
+            val textClose = view.findViewById<TextView>(R.id.textClose)
+            textClose.setOnClickListener {
+                dialog.dismiss()
             }
+            dialog.setContentView(view)
+
+            dialog.show()
+
+        }
     }
 }
